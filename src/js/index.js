38f169be5b8a36f50e9e22ba7850c15915ff97cf
george@global <george-global@git.com>
2018-01-04 22:32:55 +0800

    update_20180104

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,32 @@
-(function($){
-})(jQuery)
+(function () {
+    var carousel = new Vue({
+        el: '.carousel',
+        data: {
+            mark: 0,
+            timer: null,
+            img: ['images/scroll-1.png',
+                'images/scroll-2.png',
+                'images/scroll-3.png',
+                'images/scroll-4.png'
+            ]
+        },
+        created() {
+            this.play()
+        },
+        methods: {
+            change(i) {
+                this.mark = i
+            },
+            autoPlay() {
+                this.mark++
+                if (this.mark === 4) {
+                    this.mark = 0
+                    return
+                }
+            },
+            play() {
+                setInterval(this.autoPlay, 2000)
+            }
+        }
+    })
+})()
