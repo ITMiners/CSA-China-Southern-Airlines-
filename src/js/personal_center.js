6a0901ed3fb8fb3ce76ac72a2e84fec2be59f6eb
Tiny <455274906@qq.com>
2018-01-06 19:52:39 +0800

    finish personal_center.html

--- src/js/personal_center.js
+++ src/js/personal_center.js
@@ -1,3 +1,15 @@
 /**
  * Created by Administrator on 2018/1/6.
  */
+$(function(){
+    $('.section_li').click(function(){
+        if($(this).children('ul').css('display') === 'none'){
+            $(this).children('ul').css('display','block');
+            console.log($(this).children('.tier1 .tier1_r'))
+            $(this).children('.tier1').children('.tier1_r').css('transform','rotate(-90deg)');
+        } else{
+            $(this).children('ul').css('display','none');
+            $(this).children('.tier1').children('.tier1_r').css('transform','rotate(0deg)');
+        }
+    })
+});
